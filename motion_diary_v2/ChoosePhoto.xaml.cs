1753b3aa4b325701b8dbce83c23e60e8d2598fa8
Xuchen1591 <magina.1591@outlook.com>
2015-05-30 01:56:42 +0800

    init commit

---
+++ motion_diary_v2/ChoosePhoto.xaml.cs
@@ -0,0 +1,141 @@
+﻿using motion_diary_v2.Common;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// “基本页”项模板在 http://go.microsoft.com/fwlink/?LinkId=234237 上有介绍
+
+namespace motion_diary_v2
+{
+    /// <summary>
+    /// 基本页，提供大多数应用程序通用的特性。
+    /// </summary>
+    public sealed partial class ChoosePhoto : Page
+    {
+
+        private NavigationHelper navigationHelper;
+        private ObservableDictionary defaultViewModel = new ObservableDictionary();
+
+        /// <summary>
+        /// 可将其更改为强类型视图模型。
+        /// </summary>
+        public ObservableDictionary DefaultViewModel
+        {
+            get { return this.defaultViewModel; }
+        }
+
+        /// <summary>
+        /// NavigationHelper 在每页上用于协助导航和
+        /// 进程生命期管理
+        /// </summary>
+        public NavigationHelper NavigationHelper
+        {
+            get { return this.navigationHelper; }
+        }
+
+
+        public ChoosePhoto()
+        {
+            this.InitializeComponent();
+            this.navigationHelper = new NavigationHelper(this);
+            this.navigationHelper.LoadState += navigationHelper_LoadState;
+            this.navigationHelper.SaveState += navigationHelper_SaveState;
+        }
+
+        /// <summary>
+        ///使用在导航过程中传递的内容填充页。 在从以前的会话
+        /// 重新创建页时，也会提供任何已保存状态。
+        /// </summary>
+        /// <param name="sender">
+        /// 事件的来源; 通常为 <see cref="NavigationHelper"/>
+        /// </param>
+        /// <param name="e">事件数据，其中既提供在最初请求此页时传递给
+        /// <see cref="Frame.Navigate(Type, Object)"/> 的导航参数，又提供
+        /// 此页在以前会话期间保留的状态的
+        /// 的字典。 首次访问页面时，该状态将为 null。</param>
+        private void navigationHelper_LoadState(object sender, LoadStateEventArgs e)
+        {
+        }
+
+        /// <summary>
+        /// 保留与此页关联的状态，以防挂起应用程序或
+        /// 从导航缓存中放弃此页。  值必须符合
+        /// <see cref="SuspensionManager.SessionState"/> 的序列化要求。
+        /// </summary>
+        ///<param name="sender">事件的来源；通常为 <see cref="NavigationHelper"/></param>
+        ///<param name="e">提供要使用可序列化状态填充的空字典
+        ///的事件数据。</param>
+        private void navigationHelper_SaveState(object sender, SaveStateEventArgs e)
+        {
+        }
+
+        #region NavigationHelper 注册
+
+        /// 此部分中提供的方法只是用于使
+        /// NavigationHelper 可响应页面的导航方法。
+        /// 
+        /// 应将页面特有的逻辑放入用于
+        /// <see cref="GridCS.Common.NavigationHelper.LoadState"/>
+        /// 和 <see cref="GridCS.Common.NavigationHelper.SaveState"/> 的事件处理程序中。
+        /// 除了在会话期间保留的页面状态之外
+        /// LoadState 方法中还提供导航参数。
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            navigationHelper.OnNavigatedTo(e);
+        }
+
+        protected override void OnNavigatedFrom(NavigationEventArgs e)
+        {
+            navigationHelper.OnNavigatedFrom(e);
+        }
+
+        #endregion
+
+        private async void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Windows.Storage.Pickers.FileOpenPicker openPicker =
+                new Windows.Storage.Pickers.FileOpenPicker();
+
+            openPicker.ViewMode = Windows.Storage.Pickers.PickerViewMode.Thumbnail;
+            openPicker.SuggestedStartLocation = Windows.Storage.Pickers.PickerLocationId.Desktop;
+
+            openPicker.FileTypeFilter.Clear();
+            openPicker.FileTypeFilter.Add(".bmp");
+            openPicker.FileTypeFilter.Add(".jpg");
+            openPicker.FileTypeFilter.Add(".jpeg");
+            openPicker.FileTypeFilter.Add(".png");
+            openPicker.FileTypeFilter.Add(".gif");
+
+            Windows.Storage.StorageFile file = await openPicker.PickSingleFileAsync();
+
+            if (file != null)
+            {
+                Windows.Storage.Streams.IRandomAccessStream stram
+                    = await file.OpenAsync(Windows.Storage.FileAccessMode.Read);
+
+                Windows.UI.Xaml.Media.Imaging.BitmapImage img =
+                    new Windows.UI.Xaml.Media.Imaging.BitmapImage();
+
+                img.SetSource(stram);
+                userPhoto.Source = img;
+            }
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(NotePage));
+        }
+    }
+}
---
+++ motion_diary_v2/MainPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// “空白页”项模板在 http://go.microsoft.com/fwlink/?LinkId=234238 上有介绍
+
+
+namespace motion_diary_v2
+{
+    /// <summary>
+    /// 可用于自身或导航至 Frame 内部的空白页。
+    /// </summary> 
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            //public bool Navigate(Type sourcePageType,object parameter);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            User user = new User
+            {
+                userName = nameInput.Text
+            };
+
+            this.Frame.Navigate(typeof(ChoosePhoto),user);
+            //this.DataContext = nameInput;
+            //this.NavigationService.Navigate(LogoPage,nameInput.Text);
+        }
+    }
+}
